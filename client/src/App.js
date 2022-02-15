3c109a371f83c7da185c2afbf6295b059230aa89
Seonaid-McNabb <seonaidmcnabb@gmail.com>
2022-02-15 19:50:54 +0100

    add new branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+//comment to add push
 function App() {
   return (
     <div className="App">
