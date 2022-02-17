21ff7e27148536180ed1518c28cce081a17293bd
AlexJackson01 <ajjackson22@gmail.com>
2022-02-17 14:55:42 +0000

    Comments updated and code started to add shapes

--- client/src/components/whiteboard/drawingtool.js
+++ client/src/components/whiteboard/drawingtool.js
@@ -1,29 +1,36 @@
 import React, {useLayoutEffect, useState} from 'react';
 import rough from 'roughjs/bundled/rough.esm';
 
-const gen = rough.generator(); // generator allows user to create a drawable object for a shape later used with .draw method
+const gen = rough.generator(); // generator allows user to create a drawable object - to be used for shapes later with .draw method
 
-const createElement = (x1, y1, x2, y2) => { // returns coordinates based on position of cursor and element to be drawn
-    const line = gen.line(x1 - 10, y1 - 10, x2 - 10, y2 - 10);
-    return { x1, y1, x2, y2, line };
-}
 
 const DrawingTool = () => {
 
-    const [elements, setElements] = useState([]); // shape element state that is initially empty, stores coords of lines
-    const [drawing, setDrawing] = useState(false); // drawing state initially false
-    
+    const [elements, setElements] = useState([]); // shape state that is initially empty, stores coords of lines
+    const [drawing, setDrawing] = useState(false); // drawing state, initially false, becomes true when mouse is held and drawing starts, false again when it finishes
+    const [elementType, setElementType] = useState("line"); // state to determine which shapes are drawn by user. line is default
+
+    const createElement = (x1, y1, x2, y2) => { // returns coordinates based on position of cursor and element to be drawn
+        if (elementType === "line") {
+            const roughEl = gen.line(x1 - 10, y1 - 10, x2 - 10, y2 - 10); // - 10 allows cursor and line to be aligned - due to 10px border
+            return { x1, y1, x2, y2, roughEl };
+        } else if (elementType === "square") {
+            // const rect = gen.rectangle(100, 200, 200, 300); // (x1, y1, width, height), width = x2-x1, height = y2-y1
+        } else if (elementType === "circle") {
+            // const circle = gen.circle(500, 300, 200); // (x1, y1, diameter), diameter = 2 * (x2-x1 + y2-y1)
+        }
+    }    
 
     useLayoutEffect(() => {
         const canvas = document.getElementById('canvas');
-        const ctx = canvas.getContext('2d'); // gets above element's context - the thing which the drawings will be rendered on. 2d = creation of object with 2d rendering context
+        const ctx = canvas.getContext('2d'); // gets canvas's context = what the drawings will be rendered on. 2d = creation of object with 2d rendering context
 
         ctx.clearRect(0, 0, canvas.width, canvas.height); // clears screen each time it is re-rendered - do i want this if drawings should be saved?!
 
         const rc = rough.canvas(canvas); // rough.js variable that stores the canvas defined above
 
         elements.forEach((el) => {
-            rc.draw(el.line)
+            rc.draw(el.roughEl)
         });
 
         // three shapes created here based on coordinates system - (x1, y1 = first click, x2, y2 = when mouse is released), calculated using React event listeners - onMouseDown > onMouseMove > onMouseUp
@@ -53,11 +60,11 @@ const DrawingTool = () => {
 
         const { clientX, clientY } = e;
         console.log(clientX, clientY);
-        const index = elements.length - 1; // last element of 'elements' array
+        const index = elements.length - 1;
         const { x1, y1 } = elements[index];
         const updatedEl = createElement(x1, y1, clientX, clientY); // 2nd coordinates (x2, y2)
 
-        const copyEl = [...elements]; // update position of new element instead of previous one
+        const copyEl = [...elements]; // update position of new element
         copyEl[index] = updatedEl; // replace last index
         setElements(copyEl);
     }
@@ -68,6 +75,7 @@ const DrawingTool = () => {
             {/* canvas id allows us to use the Canvas API */}
             Canvas
         </canvas>
+
     </div>
   )
 }
