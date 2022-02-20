b160d3a2f5147da4c8fb4de829ee5a874cd4dcb4
Imogen <90554214+minoi001@users.noreply.github.com>
2022-02-20 15:49:44 +0000

    looping through quiz questions but not recording any answers yet

--- client/src/components/quiz/Play.js
+++ client/src/components/quiz/Play.js
@@ -27,15 +27,76 @@ const Play = (props) => {
       .catch((err) => console.log("Request Failed", err)); // Catch errors
   }
 
+  function nextQuestion(event) {
+    event.preventDefault();
+    if (currentQuestion == props.length) {
+      console.log("Quiz Finished");
+    } else {
+      setCurrentQuestion(currentQuestion + 1);
+    }
+  }
+
   return (
     <div>
       <h1>Quiz</h1>
       <p>Number of questions: {props.length}</p>
 
       {!props.questions[0] ? (
-        "Loading..."
+        "Starting..."
       ) : (
-        <form>{props.questions[currentQuestion - 1].question}</form>
+        <form onSubmit={(e) => nextQuestion(e)}>
+          <h6>{props.questions[currentQuestion - 1].question}</h6>
+          <select
+            id="Level"
+            name="Level"
+            onChange={(e) => props.setLevel(e.target.value)}
+          >
+            {!props.questions[currentQuestion - 1].answers.answer_a ? (
+              ""
+            ) : (
+              <option value="Answer A" defaultValue>
+                {props.questions[currentQuestion - 1].answers.answer_a}
+              </option>
+            )}
+            {!props.questions[currentQuestion - 1].answers.answer_b ? (
+              ""
+            ) : (
+              <option value="Answer B" defaultValue>
+                {props.questions[currentQuestion - 1].answers.answer_b}
+              </option>
+            )}
+            {!props.questions[currentQuestion - 1].answers.answer_c ? (
+              ""
+            ) : (
+              <option value="Answer C" defaultValue>
+                {props.questions[currentQuestion - 1].answers.answer_c}
+              </option>
+            )}
+            {!props.questions[currentQuestion - 1].answers.answer_d ? (
+              ""
+            ) : (
+              <option value="Answer D" defaultValue>
+                {props.questions[currentQuestion - 1].answers.answer_d}
+              </option>
+            )}
+            {!props.questions[currentQuestion - 1].answers.answer_e ? (
+              ""
+            ) : (
+              <option value="Answer E" defaultValue>
+                {props.questions[currentQuestion - 1].answers.answer_e}
+              </option>
+            )}
+            {!props.questions[currentQuestion - 1].answers.answer_f ? (
+              ""
+            ) : (
+              <option value="Answer F" defaultValue>
+                {props.questions[currentQuestion - 1].answers.answer_f}
+              </option>
+            )}
+          </select>
+          {/* conditionally render a finish quiz button that takes you to a results page and hide submit button if the currentQuestion == props.length */}
+          <input type="submit" value="Submit" />
+        </form>
       )}
     </div>
   );
