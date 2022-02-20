9e7efc45998466c16fa0f4ae1ef68baf9233fa72
Seonaid-McNabb <seonaidmcnabb@gmail.com>
2022-02-20 16:30:04 +0100

    add code to test jobreqs component html elements, fetch data from BE and display in list form

--- client/src/components/Personal Reflection Area/WorkReqsList.js
+++ client/src/components/Personal Reflection Area/WorkReqsList.js
@@ -1,13 +1,127 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WorkReqsList.css";
 
 function WorkReqsList() {
-  const [mustHaves, setMustHaves] = useState("");
+  const [mustHaves, setMustHaves] = useState([]);
+  const [negotiables, setNegotiables] = useState([]);
+  const [dealBreakers, setDealBreakers] = useState([]);
+  const [niceToHaves, setNicetoHaves] = useState([]);
+
+  //get all niceToHaves from backEnd
+  useEffect(() => {
+    fetch("http://localhost:5001/nice2haves-list")
+      .then((res) => {
+        if (res.ok) {
+          console.log(res);
+          return res.json();
+        } else {
+          throw new Error("Not 2xx response");
+        }
+      })
+      .then((json) => {
+        setNicetoHaves(json);
+        console.log(json);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  //Get all dealbreakers from the back-end
+  useEffect(() => {
+    fetch("http://localhost:5001/dealbreakers-list")
+      .then((res) => {
+        if (res.ok) {
+          console.log(res);
+          return res.json();
+        } else {
+          throw new Error("Not 2xx response");
+        }
+      })
+      .then((json) => {
+        setDealBreakers(json);
+        console.log(json);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  //Get all must-haves from back-end
+  useEffect(() => {
+    fetch("http://localhost:5001/must-haves-list")
+      .then((res) => {
+        if (res.ok) {
+          console.log(res);
+          return res.json();
+        } else {
+          throw new Error("Not 2xx response");
+        }
+      })
+      .then((json) => {
+        setMustHaves(json);
+        console.log(json);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  //get list of negotiables from backend
+  useEffect(() => {
+    fetch("http://localhost:5001/negotiables-list")
+      .then((res) => {
+        if (res.ok) {
+          console.log(res);
+          return res.json();
+        } else {
+          throw new Error("Not 2xx response");
+        }
+      })
+      .then((json) => {
+        setNegotiables(json);
+        console.log(json);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>Testing font</h1>
-      <h2>TESTING AGAIN</h2>
-      <p>Once upon a time I was testing a component</p>
+      <h1>My Professional Priorities</h1>
+      <p>
+        TO-DO: add description of the purpose of this component -Think about
+        making an interactive checklist Element -When user is applying /
+        considering jobs, could submit how many boxes it checks and receive
+        personalized advice--ie.keep looking, seems like a good fit?
+      </p>
+      <ul id="must-haves">
+        <h2>MUST-HAVES</h2>
+        {mustHaves.map((e) => (
+          <li> {e.must_haves} </li>
+        ))}
+      </ul>
+
+      <ul id="negotiables">
+        <h2>NEGOTIABLES</h2>
+        {negotiables.map((e) => (
+          <li>{e.negotiables}</li>
+        ))}
+      </ul>
+
+      <ul id="dealbreakers">
+        <h2>DEAL-BREAKERS</h2>
+        {dealBreakers.map((e) => (
+          <li>{e.deal_breakers}</li>
+        ))}
+      </ul>
+      <ul id="nice2have">
+        <h2>NICE TO HAVE</h2>
+        {niceToHaves.map((e) => (
+          <li>{e.nice_to_have}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- client/src/components/Personal Reflection Area/WorkReqsList.test.js
+++ client/src/components/Personal Reflection Area/WorkReqsList.test.js
@@ -8,29 +8,46 @@ Purpose: to allow the user to start organizing and thinking about their  future 
 Connects with: back-end database to store user input
 
 View should include:
---Title (name of component)
+--Title (name of component) COMPLETE
 --Description (why it's important to use this resource)
 --An interactive list divided into sections
---Section 1: Header "Must-haves"
---Section 2: Header "Negotiables"
---Section 3: Header: "Deal Breakers"
---Section 4: Header: "Would be Nice to Have"
+--Section 1: Header "Must-haves" COMPLETE
+--Section 2: Header "Negotiables" COMPLETE
+--Section 3: Header: "Deal Breakers" COMPLETE
+--Section 4: Header: "Nice to Have" COMPLETE
 
 STATES:
---[mustHaves, setMustHaves]
---[negotiables, setNegotiables]
---[dealBreakers, setDealBreakers]
---[niceToHaves, setNicetoHaves]*/
+--[mustHaves, setMustHaves] COMPLETE
+--[negotiables, setNegotiables] COMPLETE
+--[dealBreakers, setDealBreakers] COMPLETE
+--[niceToHaves, setNicetoHaves] COMPLETE*/
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe("Testing the Future Work Component", () => {
+describe("Testing the Future Work Component Rendered Elements", () => {
   let wrapper;
 
   beforeEach(() => {
     wrapper = shallow(<WorkReqsList />); //before each tests, gets the WorkReqsList component to evaluate it
   });
-  it("should have paragraph elements", () => {
-    wrapper.find("p");
+  it("should have a heading with explanatory text", () => {
+    let componentTitle = wrapper.find("h1");
+    expect(componentTitle.text()).toBe("My Professional Priorities");
+  });
+  it("should have must-have list title", () => {
+    let mustHaveTitle = wrapper.find("#must-haves");
+    expect(mustHaveTitle.text()).toBe("MUST-HAVES");
+  });
+  it("should have a negotiables list title", () => {
+    let negotiablesTitle = wrapper.find("#negotiables");
+    expect(negotiablesTitle.text()).toBe("NEGOTIABLES");
+  });
+  it("should have a deal-breakers list title", () => {
+    let dealbreakersTitle = wrapper.find("#dealbreakers");
+    expect(dealbreakersTitle.text()).toBe("DEAL-BREAKERS");
+  });
+  it("should have a nice-to-haves list title", () => {
+    let niceToHaveTitle = wrapper.find("#nice2have");
+    expect(niceToHaveTitle.text()).toBe("NICE TO HAVE");
   });
 });
