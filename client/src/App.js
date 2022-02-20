cc6937be513b27960413fbc706dc794880aadcbf
Imogen <90554214+minoi001@users.noreply.github.com>
2022-02-20 13:10:05 +0000

    start quiz form complete minus data passing

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,8 @@ import Login from "./components/Login";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Whiteboard from "./components/whiteboard/Whiteboard.js";
-
+import StartQuiz from "./components/quiz/StartQuiz";
+import Quiz from "./components/quiz/Quiz";
 
 //comment to add push
 function App() {
@@ -19,6 +20,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/whiteboard" element={<Whiteboard />} />
+        <Route path="/start-quiz" element={<StartQuiz />} />
+        <Route path="/quiz" element={<Quiz />} />
       </Routes>
 
       <Footer />
