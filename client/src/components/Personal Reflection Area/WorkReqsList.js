7a226395bd34a877c43f9f08293663f96afcce6a
Seonaid-McNabb <seonaidmcnabb@gmail.com>
2022-02-20 17:27:37 +0100

    test and add dropdown menu

--- client/src/components/Personal Reflection Area/WorkReqsList.js
+++ client/src/components/Personal Reflection Area/WorkReqsList.js
@@ -89,35 +89,50 @@ function WorkReqsList() {
 
   return (
     <div>
-      <h1>My Professional Priorities</h1>
+      <h1 id="professional-priorities-title">Professional Priorities</h1>
       <p>
         TO-DO: add description of the purpose of this component -Think about
         making an interactive checklist Element -When user is applying /
         considering jobs, could submit how many boxes it checks and receive
         personalized advice--ie.keep looking, seems like a good fit?
       </p>
-      <ul id="must-haves">
-        <h2>MUST-HAVES</h2>
+
+      <div id="add-to-list">
+        <h1 id="addNew-title">Add a new priority:</h1>
+
+        <form>
+          <select name="priority-type" id="select-type-dropdown-menu">
+            <option value="must-have">must-have</option>
+            <option value="negotiable">negotiable</option>
+            <option value="deal-breaker">deal-breaker</option>
+            <option value="nice-to-have">nice to have</option>
+          </select>
+        </form>
+      </div>
+
+      <h1 id="my-list">My List</h1>
+      <ul id="must-haves-list">
+        <h2 id="must-haves-title">MUST-HAVES</h2>
         {mustHaves.map((e) => (
           <li> {e.must_haves} </li>
         ))}
       </ul>
 
-      <ul id="negotiables">
-        <h2>NEGOTIABLES</h2>
+      <ul id="negotiables-list">
+        <h2 id="negotiables-title">NEGOTIABLES</h2>
         {negotiables.map((e) => (
           <li>{e.negotiables}</li>
         ))}
       </ul>
 
-      <ul id="dealbreakers">
-        <h2>DEAL-BREAKERS</h2>
+      <ul id="dealbreakers-list">
+        <h2 id="dealbreakers-title">DEAL-BREAKERS</h2>
         {dealBreakers.map((e) => (
           <li>{e.deal_breakers}</li>
         ))}
       </ul>
-      <ul id="nice2have">
-        <h2>NICE TO HAVE</h2>
+      <ul id="nice2have-list">
+        <h2 id="nice2have-title">NICE TO HAVE</h2>
         {niceToHaves.map((e) => (
           <li>{e.nice_to_have}</li>
         ))}
--- client/src/components/Personal Reflection Area/WorkReqsList.test.js
+++ client/src/components/Personal Reflection Area/WorkReqsList.test.js
@@ -24,6 +24,7 @@ STATES:
 
 Enzyme.configure({ adapter: new Adapter() });
 
+//TEST SUITE FOR HTML ELEMENTS/DESIGN\\
 describe("Testing the Future Work Component Rendered Elements", () => {
   let wrapper;
 
@@ -31,23 +32,40 @@ describe("Testing the Future Work Component Rendered Elements", () => {
     wrapper = shallow(<WorkReqsList />); //before each tests, gets the WorkReqsList component to evaluate it
   });
   it("should have a heading with explanatory text", () => {
-    let componentTitle = wrapper.find("h1");
-    expect(componentTitle.text()).toBe("My Professional Priorities");
+    let componentTitle = wrapper.find("#professional-priorities-title");
+    expect(componentTitle.text()).toBe("Professional Priorities");
   });
   it("should have must-have list title", () => {
-    let mustHaveTitle = wrapper.find("#must-haves");
+    let mustHaveTitle = wrapper.find("#must-haves-title");
     expect(mustHaveTitle.text()).toBe("MUST-HAVES");
   });
   it("should have a negotiables list title", () => {
-    let negotiablesTitle = wrapper.find("#negotiables");
+    let negotiablesTitle = wrapper.find("#negotiables-title");
     expect(negotiablesTitle.text()).toBe("NEGOTIABLES");
   });
   it("should have a deal-breakers list title", () => {
-    let dealbreakersTitle = wrapper.find("#dealbreakers");
+    let dealbreakersTitle = wrapper.find("#dealbreakers-title");
     expect(dealbreakersTitle.text()).toBe("DEAL-BREAKERS");
   });
   it("should have a nice-to-haves list title", () => {
-    let niceToHaveTitle = wrapper.find("#nice2have");
+    let niceToHaveTitle = wrapper.find("#nice2have-title");
     expect(niceToHaveTitle.text()).toBe("NICE TO HAVE");
+  });
+  it("should have an add new section", () => {
+    let addNewSection = wrapper.find("#addNew-title");
+    expect(addNewSection.text()).toBe("Add a new priority:");
+  });
+});
+
+//TEST SUITES FOR FUNCTIONALITY OF ELEMENTS\\
+describe("Testing the DropDown Select Type Menu", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<WorkReqsList />); //before each tests, gets the WorkReqsList component to evaluate it
+  });
+  it("should have a dropdown menu", () => {
+    let dropdownMenu = wrapper.find("#select-type-dropdown-menu");
+    expect(dropdownMenu.exists()).toEqual(true);
   });
 });
