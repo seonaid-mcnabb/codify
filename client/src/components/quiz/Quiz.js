061b45c7664dad7380fa7f964aecb811acaa22c4
Imogen <90554214+minoi001@users.noreply.github.com>
2022-02-20 17:30:31 +0000

    converted to radio buttons

--- client/src/components/quiz/Quiz.js
+++ client/src/components/quiz/Quiz.js
@@ -12,33 +12,66 @@ const StartQuiz = (props) => {
       <form>
         <div className="Topic">
           <label for="Topic">Topic:</label>
-          {/* convert this to a radio */}
-
-          <select
-            id="Topic"
+          <br />
+          <input
+            type="radio"
+            id="HTML"
             name="Topic"
-            onChange={(e) => props.setTopic(e.target.value)}
-          >
-            <option value="HTML">HTML</option>
-            <option value="JavaScript">JavaScript</option>
-            <option value="MySQL">MySQL</option>
-          </select>
+            value="HTML"
+            onClick={(e) => props.setTopic(e.target.value)}
+          />
+          <label for="html">HTML</label>
+          <br />
+          <input
+            type="radio"
+            id="JavaScript"
+            name="Topic"
+            value="JavaScript"
+            onClick={(e) => props.setTopic(e.target.value)}
+          />
+          <label for="javascript">JavaScript</label>
+          <br />
+          <input
+            type="radio"
+            id="MySQL"
+            name="Topic"
+            value="MySQL"
+            onClick={(e) => props.setTopic(e.target.value)}
+          />
+          <label for="MySQL">MySQL</label>
+          <br />
         </div>
         <br />
         <div className="Level">
           <label for="Level">Level:</label>
-          {/* convert this to a radio */}
-          <select
-            id="Level"
+          <br />
+          <input
+            type="radio"
+            id="Easy"
             name="Level"
-            onChange={(e) => props.setLevel(e.target.value)}
-          >
-            <option value="Easy" defaultValue>
-              Easy
-            </option>
-            <option value="Medium">Medium</option>
-            <option value="Hard">Hard</option>
-          </select>
+            value="Easy"
+            onClick={(e) => props.setLevel(e.target.value)}
+          />
+          <label for="Easy">Easy</label>
+          <br />
+          <input
+            type="radio"
+            id="Medium"
+            name="Level"
+            value="Medium"
+            onClick={(e) => props.setLevel(e.target.value)}
+          />
+          <label for="Medium">Medium</label>
+          <br />
+          <input
+            type="radio"
+            id="Hard"
+            name="Level"
+            value="Hard"
+            onClick={(e) => props.setLevel(e.target.value)}
+          />
+          <label for="Hard">Hard</label>
+          <br />
         </div>
         <br />
         <div className="Length">
