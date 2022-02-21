d0065edece3ca2fab3af3e577014ed220e3dcc2e
Imogen <90554214+minoi001@users.noreply.github.com>
2022-02-21 18:01:08 +0000

    new favicon

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
