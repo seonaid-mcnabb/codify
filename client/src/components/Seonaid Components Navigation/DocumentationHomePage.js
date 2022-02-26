2dae426f1389937ddc5e2d7f8b163b1e910405a3
Seonaid-McNabb <seonaidmcnabb@gmail.com>
2022-02-26 13:07:55 +0100

    add styled link to personal docs area

--- client/src/components/Seonaid Components Navigation/DocumentationHomePage.js
+++ client/src/components/Seonaid Components Navigation/DocumentationHomePage.js
@@ -3,17 +3,21 @@ import React, { useState, useEffect } from "react";
 import Header from "../Header.js";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
+import "./DocumentationHomePage.css";
 
 //This is a tab in the header that, onClick, contains links to all of the personal
 //coding documentation resources
 
 function DocumentationHomePage() {
   return (
-    <div>
+    <div className="documentation-nav">
       <Header></Header>
-      <NavLink className="padded" to="/qandas">
-        Q&As
-      </NavLink>
+
+      <container className="documentation-navigation">
+        <NavLink className="q-and-a-link" to="/qandas">
+          Q&As
+        </NavLink>
+      </container>
     </div>
   );
 }
