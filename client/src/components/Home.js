22f797af3681477e89c91169d72acd3f5a9e164e
AlexJackson01 <ajjackson22@gmail.com>
2022-02-27 20:49:30 +0000

    Started styling landing page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Codify from "./Codify.png";
 const Home = () => {
   return (
-    <div>
-      <Header />
-      <Footer />
+    <div className="landing-page">
+      {/* <Header /> */}
+      <img src={Codify} className="landing-logo" alt="Codify logo" />
+      <h1 className="landing-header">Welcome</h1>
     </div>
   );
 };
