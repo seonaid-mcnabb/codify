497a6574d63db55b1ea34a5aecb5c28edbe608a9
Imogen <90554214+minoi001@users.noreply.github.com>
2022-02-27 20:30:58 +0000

    quiz styling

--- client/src/components/quiz/Play.js
+++ client/src/components/quiz/Play.js
@@ -3,10 +3,15 @@ import { Link } from "react-router-dom";
 import "./Quiz.css";
 import Header from "../Header";
 import Footer from "../Footer";
+import { Box, Button, Center, Radio, RadioGroup } from "@chakra-ui/react";
 
 const Play = (props) => {
   useEffect(() => {
-    startQuiz();
+    if (props.quizStatus === "Finished") {
+      console.log("Finished");
+    } else {
+      startQuiz();
+    }
   }, props);
 
   let [currentQuestion, setCurrentQuestion] = useState(1);
@@ -54,149 +59,146 @@ const Play = (props) => {
   return (
     <div>
       <Header />
+      <center>
+        {props.quizStatus == "Playing" ? (
+          <div className="page">
+            <Center display="block" alignItems="center">
+              <Box
+                alignItems="flex"
+                className="Questions"
+                bg="#BFE8F3"
+                borderRadius="1rem"
+                padding="2rem"
+                maxWidth="600px"
+              >
+                {!props.questions[0] ? (
+                  "Starting..."
+                ) : (
+                  <form
+                    id={currentQuestion}
+                    name={currentQuestion}
+                    onSubmit={(e) => nextQuestion(e)}
+                  >
+                    <h1 id={currentQuestion} name={currentQuestion}>
+                      {props.questions[currentQuestion - 1].question}
+                    </h1>
+                    <RadioGroup
+                      onChange={setCurrentAnswer}
+                      value={currentAnswer}
+                    >
+                      {/* ANSWER A */}
+                      {!props.questions[currentQuestion - 1].answers
+                        .answer_a ? (
+                        ""
+                      ) : (
+                        <Radio
+                          name="Answer A"
+                          colorScheme="orange"
+                          value="answer_a"
+                          display="flex"
+                          paddingLeft="10%"
+                        >
+                          {
+                            props.questions[currentQuestion - 1].answers
+                              .answer_a
+                          }
+                        </Radio>
+                      )}
 
-      <h1>Quiz</h1>
-
-      {props.quizStatus == "Playing" ? (
-        <div>
-          <p>
-            Question {currentQuestion} of {props.length}
-          </p>
-          {!props.questions[0] ? (
-            "Starting..."
-          ) : (
-            <form
-              id={currentQuestion}
-              name={currentQuestion}
-              onSubmit={(e) => nextQuestion(e)}
-            >
-              <h6 id={currentQuestion} name={currentQuestion}>
-                {props.questions[currentQuestion - 1].question}
-              </h6>
-              {/* ANSWER A */}
-              {!props.questions[currentQuestion - 1].answers.answer_a ? (
-                ""
-              ) : (
-                <div className="Answer A">
-                  <input
-                    type="radio"
-                    id="Answer A"
-                    name={currentQuestion}
-                    value={
-                      props.questions[currentQuestion - 1].answers.answer_a
-                    }
-                    onClick={(e) => setCurrentAnswer("answer_a")}
-                  />
-                  <label for="Answer A">
-                    {props.questions[currentQuestion - 1].answers.answer_a}
-                  </label>
-                  <br />
-                </div>
-              )}
-
-              {/* ANSWER B */}
-              {!props.questions[currentQuestion - 1].answers.answer_b ? (
-                ""
-              ) : (
-                <div className="Answer B">
-                  <input
-                    type="radio"
-                    id="Answer B"
-                    name={currentQuestion}
-                    value={
-                      props.questions[currentQuestion - 1].answers.answer_b
-                    }
-                    onClick={(e) => setCurrentAnswer("answer_b")}
-                  />
-                  <label for="Answer B">
-                    {props.questions[currentQuestion - 1].answers.answer_b}
-                  </label>
-                  <br />
-                </div>
-              )}
-              {/* ANSWER C */}
-              {!props.questions[currentQuestion - 1].answers.answer_c ? (
-                ""
-              ) : (
-                <div className="Answer C">
-                  <input
-                    type="radio"
-                    id="Answer C"
-                    name={currentQuestion}
-                    value={
-                      props.questions[currentQuestion - 1].answers.answer_c
-                    }
-                    onClick={(e) => setCurrentAnswer("answer_c")}
-                  />
-                  <label for="Answer C">
-                    {props.questions[currentQuestion - 1].answers.answer_c}
-                  </label>
-                  <br />
-                </div>
-              )}
-              {/* ANSWER D */}
-              {!props.questions[currentQuestion - 1].answers.answer_d ? (
-                ""
-              ) : (
-                <div className="Answer D">
-                  <input
-                    type="radio"
-                    id="Answer D"
-                    name={currentQuestion}
-                    value={
-                      props.questions[currentQuestion - 1].answers.answer_d
-                    }
-                    onClick={(e) => setCurrentAnswer("answer_d")}
-                  />
-                  <label for="Answer D">
-                    {props.questions[currentQuestion - 1].answers.answer_d}
-                  </label>{" "}
-                  <br />
-                </div>
-              )}
-              {/* ANSWER E */}
-              {!props.questions[currentQuestion - 1].answers.answer_e ? (
-                ""
-              ) : (
-                <div className="Answer E">
-                  <input
-                    type="radio"
-                    id="Answer E"
-                    name={currentQuestion}
-                    value={
-                      props.questions[currentQuestion - 1].answers.answer_e
-                    }
-                    onClick={(e) => setCurrentAnswer("answer_e")}
-                  />
-                  <label for="Answer E">
-                    {props.questions[currentQuestion - 1].answers.answer_e}
-                  </label>{" "}
-                  <br />
-                </div>
-              )}
-              {/* ANSWER F */}
-              {!props.questions[currentQuestion - 1].answers.answer_f ? (
-                ""
-              ) : (
-                <div className="Answer F">
-                  <input
-                    type="radio"
-                    id="Answer F"
-                    name={currentQuestion}
-                    value={
-                      props.questions[currentQuestion - 1].answers.answer_f
-                    }
-                    onClick={(e) => setCurrentAnswer("answer_f")}
-                  />
-                  <label for="Answer F">
-                    {props.questions[currentQuestion - 1].answers.answer_f}
-                  </label>{" "}
-                  <br />
-                </div>
-              )}
-              {/* conditionally render a finish quiz button that takes you to a results page and hide submit button if the currentQuestion == props.length */}
-              <br />
-              {/* {currentQuestion == 1 ? (
+                      {/* ANSWER B */}
+                      {!props.questions[currentQuestion - 1].answers
+                        .answer_b ? (
+                        ""
+                      ) : (
+                        <Radio
+                          name="Answer B"
+                          colorScheme="orange"
+                          value="answer_b"
+                          display="flex"
+                          paddingLeft="10%"
+                        >
+                          {
+                            props.questions[currentQuestion - 1].answers
+                              .answer_b
+                          }
+                        </Radio>
+                      )}
+                      {/* ANSWER C */}
+                      {!props.questions[currentQuestion - 1].answers
+                        .answer_c ? (
+                        ""
+                      ) : (
+                        <Radio
+                          name="Answer C"
+                          colorScheme="orange"
+                          value="answer_c"
+                          display="flex"
+                          paddingLeft="10%"
+                        >
+                          {
+                            props.questions[currentQuestion - 1].answers
+                              .answer_c
+                          }
+                        </Radio>
+                      )}
+                      {/* ANSWER D */}
+                      {!props.questions[currentQuestion - 1].answers
+                        .answer_d ? (
+                        ""
+                      ) : (
+                        <Radio
+                          name="Answer D"
+                          colorScheme="orange"
+                          value="answer_d"
+                          display="flex"
+                          paddingLeft="10%"
+                        >
+                          {
+                            props.questions[currentQuestion - 1].answers
+                              .answer_d
+                          }
+                        </Radio>
+                      )}
+                      {/* ANSWER E */}
+                      {!props.questions[currentQuestion - 1].answers
+                        .answer_e ? (
+                        ""
+                      ) : (
+                        <Radio
+                          name="Answer E"
+                          colorScheme="orange"
+                          value="answer_e"
+                          display="flex"
+                          paddingLeft="10%"
+                        >
+                          {
+                            props.questions[currentQuestion - 1].answers
+                              .answer_e
+                          }
+                        </Radio>
+                      )}
+                      {/* ANSWER F */}
+                      {!props.questions[currentQuestion - 1].answers
+                        .answer_f ? (
+                        ""
+                      ) : (
+                        <Radio
+                          name="Answer F"
+                          colorScheme="orange"
+                          value="answer_f"
+                          display="flex"
+                          paddingLeft="10%"
+                        >
+                          {
+                            props.questions[currentQuestion - 1].answers
+                              .answer_f
+                          }
+                        </Radio>
+                      )}
+                    </RadioGroup>
+                    {/* conditionally render a finish quiz button that takes you to a results page and hide submit button if the currentQuestion == props.length */}
+                    <br />
+                    {/* {currentQuestion == 1 ? (
                 ""
               ) : (
                 <input
@@ -206,18 +208,35 @@ const Play = (props) => {
                 />
               )} */}
 
-              {currentQuestion < props.length ? (
-                <input type="submit" value="Next" />
-              ) : (
-                <input type="submit" value="Submit Answers" />
-              )}
-            </form>
-          )}{" "}
-        </div>
-      ) : (
-        <Link to="/quiz/results">Get my results!</Link>
-      )}
-      <Footer />
+                    {currentQuestion < props.length ? (
+                      <Button>
+                        <input type="submit" value="Next" />
+                      </Button>
+                    ) : (
+                      <Button>
+                        <input type="submit" value="Submit Answers" />
+                      </Button>
+                    )}
+                  </form>
+                )}{" "}
+                <p className="right bottom">
+                  Question {currentQuestion} of {props.length}
+                </p>
+              </Box>
+            </Center>
+          </div>
+        ) : (
+          <div>
+            <p className="center">
+              How do you think you did? Well, now it's time to find out!
+            </p>
+            <Button bg="#BFE8F3" color="orange" padding="4rem">
+              <Link to="/quiz/results">Get my results!</Link>
+            </Button>
+          </div>
+        )}
+        <Footer />
+      </center>
     </div>
   );
 };
